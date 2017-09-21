c947947e13ec63a70d0b799919d8880b2070097b
Baudin-Laurencin <abaudin@gmail.com>
2017-09-21 13:12:44 +0000

    Formulaire mis en form et fonctionel

--- insertIntoPersons.php
+++ insertIntoPersons.php
@@ -4,34 +4,28 @@ server with default setting (user 'root' with no password) */
 
     $servername = getenv('IP');
     $username = getenv('C9_USER');
-    $password = "";
-    $database = "db";
+    $password = '';
+    $database = 'db';
     $dbport = 3306;
     $link = mysqli_connect($servername, $username, $password, $database);
-    $link->set_charset("utf8");
+    $link->set_charset('utf8');
 // Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+if ($link === false) {
+    die('ERROR: Could not connect. '.mysqli_connect_error());
 }
-
 
 // Escape user inputs for security
 
-
 $civil = isset($_POST['selectCivil']) ? true : false; //toujours basé sur l'attribut name du select
-if($civil) {
-   $value = htmlentities($_POST['selectCivil'], ENT_QUOTES, "UTF-8");//permet de protéger tout les caractères spéciaux
+if ($civil) {
+    $value = htmlentities($_POST['selectCivil'], ENT_QUOTES, 'UTF-8'); //permet de protéger tout les caractères spéciaux
    //ensuite requête pour ajouter en base ...
 } else {
-  echo "Select Civil is required";
-  exit;
+    echo 'Select Civil is required';
+    exit;
 }
 
-
 $civil = mysqli_real_escape_string($link, $_REQUEST['selectCivil']);
-
-
-
 
 $first_name = mysqli_real_escape_string($link, $_REQUEST['first_name']);
 $last_name = mysqli_real_escape_string($link, $_REQUEST['last_name']);
@@ -39,14 +33,14 @@ $email = mysqli_real_escape_string($link, $_REQUEST['email']);
 
 // attempt insert query execution
 $sql = "INSERT INTO persons (first_name, last_name, email,civil) VALUES ('$first_name', '$last_name', '$email','$civil')";
-if(mysqli_query($link, $sql)){
+if (mysqli_query($link, $sql)) {
     echo "Records added successfully. <br>
     ___________________<a href='form.php' rel='nofollow'>retour</a>";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+} else {
+    echo "ERROR: Could not able to execute $sql. ".mysqli_error($link);
+
+    include 'modal.html';
 }
 
     // close connection
 mysqli_close($link);
-
-?>
